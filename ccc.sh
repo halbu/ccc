8ae724561f1c41623d7d6bf9a815697fac38ee95
halbu <fstream.c@gmail.com>
2019-05-31 17:47:26 +0100

    Remove WIP code

--- ccc.sh
+++ ccc.sh
@@ -70,15 +70,6 @@ today() {
   printf "\nTotal calories today: %s\n" $s
 }
 
-graphweight() {
-  q=($(sqlite3 ccc.db "select * from weight where date >= datetime('now', '-28 days');"))
-  w=$(echo "${q[*]}" | tr ' ' '\n' | awk 'BEGIN{FS="|"} {print $1}')
-  min=$(echo "${w[*]}" | awk 'BEGIN{a=999} {if ($1<0+a) a=$1} END{print a}')
-  max=$(echo "${w[*]}" | awk 'BEGIN{a=0  } {if ($1>0+a) a=$1} END{print a}')
-  echo min.. $min
-  echo max.. $max
-}
-
 yesterday() {
   y=$(date -d "yesterday" '+%Y-%m-%d')
   d=$(date -d "today" '+%Y-%m-%d')
@@ -99,7 +90,7 @@ yesterday() {
   printf "\nTotal calories yesterday: %s\n" $s
 }
 
-while getopts 'a:q:l:w:typg' flag; do
+while getopts 'a:q:l:w:typ' flag; do
   case "${flag}" in
     l) logfood $OPTARG ;;
     a) addfood $OPTARG ;;
@@ -108,6 +99,5 @@ while getopts 'a:q:l:w:typg' flag; do
     t) today ;;
     y) yesterday ;;
     w) weight $OPTARG ;;
-    g) graphweight ;;
   esac
 done
